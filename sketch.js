de18b23824438a77ec6335a6d898c31f8ed9fa06
RidaZeya <89671221+RidaZeya@users.noreply.github.com>
2021-09-04 10:59:49 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,21 +20,21 @@ var gameOver, restart;
 
 function preload(){
   pathImg = loadImage("PATH.jpg");
-  mainRacerImg1 = loadAnimation("images/mainPlayer1.png","images/mainPlayer2.png");
-  mainRacerImg2= loadImage("images/mainPlayer2.png");
-  mainRacerImg3 = loadImage("images/mainPlayer3.png");
+  mainRacerImg1 = loadAnimation("mainPlayer1.png","images/mainPlayer2.png");
+  mainRacerImg2= loadImage("mainPlayer2.png");
+  mainRacerImg3 = loadImage("mainPlayer3.png");
   
-  oppPink1Img = loadAnimation("images/opponent1.png","images/opponent2.png");
-  oppPink2Img = loadAnimation("images/opponent3.png");
+  oppPink1Img = loadAnimation("opponent1.png","images/opponent2.png");
+  oppPink2Img = loadAnimation("opponent3.png");
   
-  oppYellow1Img = loadAnimation("images/opponent4.png","images/opponent5.png");
-  oppYellow2Img = loadAnimation("images/opponent6.png");
+  oppYellow1Img = loadAnimation("opponent4.png","images/opponent5.png");
+  oppYellow2Img = loadAnimation("opponent6.png");
   
-  oppRed1Img = loadAnimation("images/opponent7.png","images/opponent8.png");
-  oppRed2Img = loadAnimation("images/opponent9.png");
+  oppRed1Img = loadAnimation("opponent7.png","images/opponent8.png");
+  oppRed2Img = loadAnimation("opponent9.png");
   
   cycleBell = loadSound("sound/bell.mp3");
-  gameOverImg = loadImage("images/gameOver.png");
+  gameOverImg = loadImage("gameOver.png");
   UPARROWIMG = loadImage("UP ARROW.png");
 }
 
